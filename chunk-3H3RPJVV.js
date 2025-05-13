bebb617378e6f5439ef2ba13db44069330b9607e
ReyUmilde <reyli.martinezh@gmail.com>
2025-05-13 11:58:34 -0600

    Auto-generated commit

---
+++ chunk-3H3RPJVV.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-FU4LXEZQ.js";import"./chunk-ZC2DJNZL.js";export{a as MaquinaComponent};
---
+++ chunk-XQYBNWSH.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-UV7YGLRU.js";import"./chunk-ZC2DJNZL.js";export{a as EntrenamientoComponent};
